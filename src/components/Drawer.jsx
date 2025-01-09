cc7843ba52bd78adb7c1496581e8571eb2215e40
Ugyen Wangda Gyeltshen <ugyenwgyeltshen.ug@gmail.com>
2025-01-09 16:13:45 +0600

    drawer update + router

--- src/components/Drawer.jsx
+++ src/components/Drawer.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import Button from "./Button";
 
 export default function Drawer() {
@@ -6,49 +7,59 @@ export default function Drawer() {
       Welcome, User
       <ul className="drawer-list">
         <li>
-          <Button
-            text={
-              <>
-                <i className="fas fa-tachometer-alt"></i> Dashboard
-              </>
-            }
-          />
+          <Link to="/" className="no-underline">
+            <Button
+              text={
+                <>
+                  <i className="fas fa-tachometer-alt"></i> Dashboard
+                </>
+              }
+            />
+          </Link>
         </li>
         <li>
-          <Button
-            text={
-              <>
-                <i className="fas fa-user"></i> User
-              </>
-            }
-          />
+          <Link to="/usermanagement" className="no-underline">
+            <Button
+              text={
+                <>
+                  <i className="fas fa-user"></i> User
+                </>
+              }
+            />
+          </Link>
         </li>
         <li>
-          <Button
-            text={
-              <>
-                <i className="fas fa-building"></i> Agency
-              </>
-            }
-          />
+          <Link to="/agency" className="no-underline">
+            <Button
+              text={
+                <>
+                  <i className="fas fa-building"></i> Agency
+                </>
+              }
+            />
+          </Link>
         </li>
         <li>
-          <Button
-            text={
-              <>
-                <i className="fas fa-key"></i> Permissions
-              </>
-            }
-          />
+          <Link to="/permissions" className="no-underline">
+            <Button
+              text={
+                <>
+                  <i className="fas fa-key"></i> Permissions
+                </>
+              }
+            />
+          </Link>
         </li>
         <li>
-          <Button
-            text={
-              <>
-                <i className="fas fa-user-shield"></i> Roles
-              </>
-            }
-          />
+          <Link to="/roles" className="no-underline">
+            <Button
+              text={
+                <>
+                  <i className="fas fa-user-shield"></i> Roles
+                </>
+              }
+            />
+          </Link>
         </li>
       </ul>
     </div>
